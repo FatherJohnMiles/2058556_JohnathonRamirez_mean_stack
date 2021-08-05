4ea89c55a451e3158a060c26fe5cee81ec074b26
Johnathan.R <101jmramirez@gmail.com>
2021-08-04 21:56:30 -0500

    adding TBP

---
+++ Phase 1/Team_Budget_Planner_app/appscript.js
@@ -0,0 +1,45 @@
+const addto = e => {
+    
+    
+    let newdata ={
+        new_client: document.getElementById('ClientN').value,
+        new_project: document.getElementById('ProjectN').value,
+        new_budget: document.getElementById('NBudget').value,
+
+    }
+    
+    localStorage.setItem('newdata', JSON.stringify(newdata));
+    displayData();
+    e.preventDefault();
+}
+
+function dispData()
+{
+    if(localStorage.getItem('newdata'))
+    {
+        let {new_client, new_project, new_budget} = JSON.parse(localStorage.getItem('newdata'));
+        var updated = document.getElementById('updated');
+        updated.innerHTML = `
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Client Name</th>
+                        <th>Project Name</th>
+                        <th>Budget</th>
+                    </tr>
+                 <tr>
+                        <td>${new_client}</td>
+                         <td>${new_project}</td>
+                         <td>${new_budget}</td>
+                    </tr>
+                 </tbody>
+            </table>
+    
+        `;
+
+    }
+
+
+}
+
+    displayData();
